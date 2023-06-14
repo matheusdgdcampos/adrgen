d5b476612c99e722d111fcf67824bdfbf725f051
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2023-06-14 01:26:28 -0300

    fix: adapt function to accept multi language

--- lib/set-title.ts
+++ lib/set-title.ts
@@ -1,3 +1,11 @@
+import { Message } from "./errors/message";
+
 export function setTitle(title: string, text_content: string) {
+  if (title.length === 0) {
+    Message.error('Title param must be more than 1 character');
+    Message.info('Title param must be more than 1 character');
+    return process.exit(1);
+  }
+
   return text_content.replace(/\[title\]/, title);
 }
