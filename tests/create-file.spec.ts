9df9513a7d55aca2e4ae7b91d4ee92fab53a45b2
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2024-05-26 17:23:02 -0300

    test: improve tests

--- tests/create-file.spec.ts
+++ tests/create-file.spec.ts
@@ -1,19 +1,22 @@
 import { describe, it, expect, afterEach } from 'vitest';
-import { readFile, rm } from 'fs/promises';
+import fs from 'node:fs/promises';
+import path from 'node:path';
 
 import { createFile } from '../lib/create-file';
 
 describe('createFile', () => {
-  const fileName = 'vitest';
-  const fileContent = 'testing file creation';
+    const fileName = 'vitest';
+    const fileContent = 'testing file creation';
+    const tempDocsDirectoryPath = path.resolve(process.cwd(), 'docs');
 
-  afterEach(async () => {
-    await rm(`${process.cwd()}/docs`, { recursive: true });
-  })
+    afterEach(async () => {
+        await fs.rm(tempDocsDirectoryPath, { recursive: true });
+    })
 
-  it('Should be able to create new file of doc', async () => {
-    await createFile(fileName, fileContent);
-    const content = await readFile(`${process.cwd()}/docs/0001-${fileName}.md`, { encoding: 'utf-8' });
-    expect(content).toEqual(fileContent);
-  })
+    it('Should be able to create new file of doc', async () => {
+        await createFile(fileName, fileContent);
+        const contentPath = path.resolve(process.cwd(), 'docs', `0001-${fileName}.md`);
+        const content = await fs.readFile(contentPath, { encoding: 'utf-8' });
+        expect(content).toEqual(fileContent);
+    })
 })
---
+++ tests/set-consequences.spec.ts
@@ -0,0 +1,25 @@
+import { describe, expect, it, beforeAll } from 'vitest';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+import { setConsequence } from '../lib/set-consequences';
+
+describe('setConsequence', () => {
+    let templateContent: string;
+    const templateFilePath = path.resolve(process.cwd(), 'lib', 'template', 'template.md');
+
+    beforeAll(async () => {
+        templateContent = await fs.readFile(templateFilePath, { encoding: 'utf-8' });
+    });
+
+    it('Should be able to generate record with consequence sentence', () => {
+        const consequenceVar = 'any consequence';
+        const consequence = setConsequence(consequenceVar, templateContent);
+        expect(consequence).toBeDefined()
+        expect(consequence?.match(consequenceVar)).toHaveLength(1);
+    });
+
+    it('Should not be able to generate consequence sentence', () => {
+        expect(setConsequence('', templateContent)).toBeUndefined();
+    });
+});
